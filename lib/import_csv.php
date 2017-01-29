966ef5f0a75e9a84496591b385278bf8ba0d00e6
Cian911 <cian@ciangallagher.net>
2017-01-29 16:31:21 +0000

    Finished CSV file parser and added database model

--- lib/import_csv.php
+++ lib/import_csv.php
@@ -11,8 +11,25 @@ class ImportCSV {
 
   public function parseCSVFile() {
     $csv_data = array_map('str_getcsv', file($this->csv_file, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES));
+    
+    // Remove first two elements
+    unset($csv_data[0]);
+    unset($csv_data[1]);
 
-    echo '<pre>';print_r( $csv_data );echo '</pre>';
+    // Re-index keys
+    $csv_data = array_values($csv_data);
+
+    foreach( $csv_data as &$data ) {
+      if($data[0] === ' ') {
+        unset($data[0]);
+      }
+      if($data[1] === ' ') {
+        unset($data[1]);
+      }
+    }
+
+    // Re-index multidiemensional array keys
+    return $csv_data = array_map(create_function('$x','$k = key($x); return (is_numeric($k)) ? array_values($x) : $x;'), $csv_data);
   }
 
 }
--- lib/model/database.php
+++ lib/model/database.php
@@ -6,4 +6,50 @@ class Database extends PDO {
     parent::__construct(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
   }
 
+  /**
+  * @param $data
+  * Expected in array format 
+  */
+  protected function insert( $data ) {
+    $query = $this->prepare("INSERT INTO stats (region, year, crime, number_of_offences)
+      VALUES ({$data['region']}, {$data['year']}, {$data['crime']}, {$data['number_of_offences']})");
+    $result = $query->execute();
+  }
+
+  protected function selectAll() {
+    $query = $this->prepare("SELECT * FROM stats");
+    $result = $query->execute();
+
+    if( $result ) {
+      return $data = $query->fetchAll();
+    }
+  }
+
+  protected function selectByYear( $year ) {
+    $query = $this->prepare("SELECT * FROM stats WHERE year = {$year}");
+    $result = $query->execute();
+
+    if( $result ) {
+      return $data = $query->fetchAll();
+    }
+  }
+
+  protected function selectByCrime( $crime ) {
+    $query = $this->prepare("SELECT * FROM stats WHERE crime = {$crime}");
+    $result = $query->execute();
+
+    if( $result ) {
+      return $data = $query->fetchAll();
+    }
+  }
+
+  protected function selectByRegion( $region ) {
+    $query = $this->prepare("SELECT * FROM stats WHERE region = {$region}");
+    $result = $query->execute();
+
+    if( $result ) {
+      return $data = $query->fetchAll();
+    }
+  }
+
 }
