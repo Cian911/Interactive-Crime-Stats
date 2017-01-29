08b6c846b05deae2ed94380fdf3fd2c0861ebe9f
Cian911 <cian@ciangallagher.net>
2017-01-29 17:14:07 +0000

    Added new function to import class to format data correctly for DB insertion

--- lib/import_csv.php
+++ lib/import_csv.php
@@ -11,7 +11,7 @@ class ImportCSV {
 
   public function parseCSVFile() {
     $csv_data = array_map('str_getcsv', file($this->csv_file, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES));
-    
+
     // Remove first two elements
     unset($csv_data[0]);
     unset($csv_data[1]);
@@ -30,6 +30,29 @@ class ImportCSV {
 
     // Re-index multidiemensional array keys
     return $csv_data = array_map(create_function('$x','$k = key($x); return (is_numeric($k)) ? array_values($x) : $x;'), $csv_data);
+  }
+
+  public function formatData( $csv_data ) {
+    $region = explode(',', $csv_data[1][0]);
+    $year = $csv_data[0][0];
+
+    unset($csv_data[0]);
+    unset($csv_data[1]);
+    $csv_data = array_values($csv_data);
+
+    $import_data = array();
+
+    foreach( $csv_data as $import ) {
+      $import_data[] = array(
+        'year'                => $year,
+        'region'              => $region[0],
+        'crime'               => substr($import[0], 4),
+        'number_of_offences'  => $import[1]
+      );
+    }
+
+    return $import_data;
+
   }
 
 }
--- lib/init.php
+++ lib/init.php
@@ -4,4 +4,7 @@ require "import_csv.php";
 
 $file = "../tests/files/raheny-stats.csv";
 $importer = new ImportCSV($file);
-$importer->parseCSVFile();
+$csv_data = $importer->parseCSVFile();
+$formatted_data = $importer->formatData($csv_data);
+
+echo '<pre>';print_r( $formatted_data );echo '</pre>';
